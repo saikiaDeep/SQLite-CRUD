c6637ce3b7a5d6110859beba22eac51fd02ef143
ds404 <deepaltmail123@gmail.com>
2022-11-08 02:11:25 +0530

    UPDATE AND DELETE

--- app/src/main/java/com/deepsaikia/updatedsqlite/CourseRVAdapter.java
+++ app/src/main/java/com/deepsaikia/updatedsqlite/CourseRVAdapter.java
@@ -1,6 +1,7 @@
 package com.deepsaikia.updatedsqlite;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +36,25 @@ public class CourseRVAdapter extends RecyclerView.Adapter<CourseRVAdapter.ViewHo
         holder.courseDescTV.setText(modal.getCourseDescription());
         holder.courseDurationTV.setText(modal.getCourseDuration());
         holder.courseTracksTV.setText(modal.getCourseTracks());
+        // below line is to add on click listener for our recycler view item.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // on below line we are calling an intent.
+                Intent i = new Intent(context, UpdateCourseActivity.class);
+
+                // below we are passing all our values.
+                i.putExtra("name", modal.getCourseName());
+                i.putExtra("description", modal.getCourseDescription());
+                i.putExtra("duration", modal.getCourseDuration());
+                i.putExtra("tracks", modal.getCourseTracks());
+
+                // starting our activity.
+                context.startActivity(i);
+            }
+        });
+
 
     }
 
@@ -42,6 +62,7 @@ public class CourseRVAdapter extends RecyclerView.Adapter<CourseRVAdapter.ViewHo
     public int getItemCount() {
         return courseModalArrayList.size();
     }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         // creating variables for our text views.
